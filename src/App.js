a89e377cd95b800fcce9d30e00dbbf568c432350
scarperez7 <95876497+scarperez7@users.noreply.github.com>
2022-03-29 18:21:40 -0400

    created react app, cleaned up files, styled header

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-center p-5">Dictionary</h1>
+      </header>
+    </div>
+  );
+}
